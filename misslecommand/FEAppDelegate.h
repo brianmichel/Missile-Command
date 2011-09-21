cce353e344a178c725bfc32b73297bc398a65116
Brian Michel <brian.michel@gmail.com>
2011-09-21 18:54:33 -0400

    Initial Commit

---
+++ misslecommand/FEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FEAppDelegate.h
+//  misslecommand
+//
+//  Created by Brian Michel on 9/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FEAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (strong) IBOutlet NSWindow *window;
+
+@end
---
+++ misslecommand/FEAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  FEAppDelegate.m
+//  misslecommand
+//
+//  Created by Brian Michel on 9/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FEAppDelegate.h"
+
+@implementation FEAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
